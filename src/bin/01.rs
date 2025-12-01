b78b07416216b2164c61f7791040cba41f42b801
Debbie Dragoness <debbiedragoness@gmail.com>
2025-12-01 15:54:25 +0000

    Simplified some code, now works if there are blank lines

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,38 +3,46 @@ advent_of_code::solution!(1);
 fn parse_instruction(s: &str) -> Option<(char, i64)> {
     let mut chars = s.chars();
     let dir = chars.next()?;
-    let num: i64 = chars.as_str().parse().ok()?; 
+    let num: i64 = chars.as_str().parse().ok()?;
 
     Some((dir, num))
 }
 
+fn parse_all_instructions(input: &str) -> Vec<(char, i64)> {
+    let input_lines: std::str::Split<'_, &str> = input.split("\n");
+    input_lines
+        .into_iter()
+        .map(|instruction| parse_instruction(instruction))
+        .into_iter()
+        .filter_map(|x| x)
+        .collect()
+}
+
 fn do_rotations(input: &str, count_all: bool) -> Option<i64> {
-    let input_lines = input.split("\n");
+    let instructions = parse_all_instructions(input);
     let mut value: i64 = 50;
     let mut next_value: i64;
-    let mut count_zeros: i64 = 0;
-    let instructions = input_lines
-        .into_iter()
-        .map(|instruction| parse_instruction(instruction).unwrap())
-        .collect::<Vec<(char, i64)>>();
+    let mut part_1: i64 = 0;
+    let mut part_2: i64 = 0;
     for (instruction, move_number) in instructions {
         match instruction {
             'R' => next_value = value + move_number,
             _ => next_value = value - move_number,
         }
         let remainder = next_value.rem_euclid(100);
-        if count_all {
-            count_zeros += next_value.abs().div_euclid(100);
-            if value != 0 && next_value <= 0 {
-                count_zeros += 1
-            }
+        part_2 += next_value.abs().div_euclid(100);
+        if value != 0 && next_value <= 0 {
+            part_2 += 1
         }
-        else {
-            if remainder == 0 {count_zeros += 1}
+        if remainder == 0 {
+            part_1 += 1
         }
         value = remainder;
     }
-    Some(count_zeros)
+    match count_all {
+        true => Some(part_2),
+        false => Some(part_1),
+    }
 }
 
 pub fn part_one(input: &str) -> Option<i64> {
