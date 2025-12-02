4764b1d94a2e6f811920db0582d51f20855a5578
Debbie Dragoness <debbiedragoness@gmail.com>
2025-12-02 07:31:49 +0000

    Day 2 done

---
+++ src/bin/02.rs
@@ -0,0 +1,93 @@
+use advent_of_code::template::commands::all;
+
+advent_of_code::solution!(2);
+
+fn parse_ranges(input: &str) -> Vec<(u64, u64)> {
+    input
+        .split(',')
+        .filter_map(|s| Some(s))
+        .map(|range| {
+            let mut parts = range.split('-');
+            let start = parts.next().unwrap().parse::<u64>().unwrap();
+            let end = parts.next().unwrap().parse::<u64>().unwrap();
+            (start, end)
+        })
+        .collect()
+}
+
+fn is_valid_half(id: &str) -> bool {
+    if id.len() % 2 != 0 {
+        return false;
+    };
+    let half_len = id.len().div_euclid(2);
+    let (first_half, second_half) = id.split_at(half_len);
+    return first_half == second_half;
+}
+
+fn is_valid_all_lengths(id: &str) -> bool {
+    let as_bytes = id.as_bytes();
+    let max_len = as_bytes.len().div_euclid(2);
+    for chunk_size in 1..=max_len {
+        if as_bytes.len() % chunk_size == 0 {
+            let first = &as_bytes[..chunk_size];
+            if as_bytes.chunks(chunk_size).all(|chunk| chunk == first) {
+                return true;
+            };
+        }
+    }
+    false
+}
+
+fn is_valid(id: &str, all_lengths: bool) -> bool {
+    if all_lengths {
+        return is_valid_all_lengths(id);
+    }
+    is_valid_half(id)
+}
+
+fn count_valid(range: (u64, u64), all_lengths: bool) -> u64 {
+    let mut count = 0;
+    for value in range.0..=range.1 {
+        if is_valid(&value.to_string(), all_lengths) {
+            count += value;
+        }
+    }
+    count
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let parsed_ranges = parse_ranges(input);
+    Some(
+        parsed_ranges
+            .into_iter()
+            .map(|range| count_valid(range, false))
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let parsed_ranges = parse_ranges(input);
+    Some(
+        parsed_ranges
+            .into_iter()
+            .map(|range| count_valid(range, true))
+            .sum(),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(1227775554));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(4174379265));
+    }
+}
