4c3f8a485f43c710bf10be2339f8925a25464787
Debbie Dragoness <debbiedragoness@gmail.com>
2025-12-03 08:33:35 +0000

    Day 3 solved!

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -17,28 +17,42 @@ fn parse_input(input: &str) -> Vec<Vec<u64>> {
         .collect()
 }
 
-fn compute_max_joltage(input: &Vec<u64>) -> u64 {
-    let (position, value_1) = input[..input.len() - 1]
+fn compute_max_joltage(input: &Vec<u64>, length: usize) -> u64 {
+    let mut joltage: u64 = 0;
+    let mut position = 0;
+    for digit_counter in (0..length).rev() {
+        let remaining_digits: Vec<&u64> = input[position..].iter().collect();
+
+        let mut best: (usize, u64) = (0, 0);
+        for (i, &value) in remaining_digits[..remaining_digits.len() - digit_counter]
+            .iter()
+            .enumerate()
+        {
+            match best {
+                (_, max_val) if *value > max_val => best = (i, *value),
+                _ => {} // ignore equal values — keeps the FIRST
+            }
+        }
+
+        position += best.0 + 1;
+        joltage += (10 as u64).pow(digit_counter as u32) * best.1;
+    }
+    joltage
+}
+
+fn sum_joltages(input: &str, length: usize) -> u64 {
+    parse_input(input)
         .iter()
-        .rev()
-        .enumerate()
-        .max_by_key(|&(_, value)| value)
-        .unwrap();
-    let value_2 = input[input.len() - position - 1..].iter().max().unwrap();
-    value_1 * 10 + value_2
+        .map(|line| compute_max_joltage(line, length))
+        .sum()
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    Some(
-        parse_input(input)
-            .iter()
-            .map(|line| compute_max_joltage(line))
-            .sum(),
-    )
+    Some(sum_joltages(input, 2))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    Some(sum_joltages(input, 12))
 }
 
 #[cfg(test)]
@@ -54,6 +68,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
