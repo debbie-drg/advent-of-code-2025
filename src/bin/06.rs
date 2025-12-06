7c7adc1ba93bd0ce3c885520ed29c756e1613eb1
Debbie Dragoness <debbiedragoness@gmail.com>
2025-12-06 12:46:53 +0000

    Made a lot cleaner

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,63 +1,77 @@
+use std::iter::zip;
+
 advent_of_code::solution!(6);
 
-pub fn part_one(input: &str) -> Option<i64> {
-    let (numbers_text, operations) = input.trim().rsplit_once("\n").unwrap();
-    let operations = operations.split_whitespace().collect::<Vec<&str>>();
-    let parsed_numbers: Vec<Vec<i64>> = numbers_text
+fn operate(operand: char, numbers: Vec<u64>) -> u64 {
+    match operand {
+        '*' => numbers
+            .iter()
+            .copied()
+            .reduce(|num_1, num_2| num_1 * num_2)
+            .unwrap(),
+        '+' => numbers.iter().sum(),
+        _ => panic!("Impossible operand"),
+    }
+}
+
+fn operate_all(operands: Vec<char>, numbers_list: Vec<Vec<u64>>) -> u64 {
+    zip(operands, numbers_list)
+        .into_iter()
+        .map(|(operand, numbers)| operate(operand, numbers))
+        .sum()
+}
+
+fn transpose<T>(twod_array: Vec<Vec<T>>) -> Vec<Vec<T>>
+where
+    T: Copy,
+{
+    (0..twod_array[0].len())
+        .map(|col| {
+            (0..twod_array.len())
+                .map(|row| twod_array[row][col])
+                .collect()
+        })
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let (numbers_text, operands) = input.trim().rsplit_once("\n").unwrap();
+    let operands = operands.chars().filter(|char| char != &' ').collect();
+    let parsed_numbers: Vec<Vec<u64>> = numbers_text
         .split("\n")
         .into_iter()
         .map(|line| {
             line.trim()
                 .split_whitespace()
-                .map(|entry| entry.parse::<i64>().unwrap())
+                .map(|entry| entry.parse::<u64>().unwrap())
                 .collect()
         })
         .collect();
-
-    Some(
-        operations
-            .into_iter()
-            .enumerate()
-            .map(|(index, operation)| match operation {
-                "*" => {
-                    let mut result: i64 = 1;
-                    for number in &parsed_numbers {
-                        result *= number[index];
-                    }
-                    result
-                }
-                "+" => {
-                    let mut result: i64 = 0;
-                    for number in &parsed_numbers {
-                        result += number[index];
-                    }
-                    result
-                }
-                _ => panic!(),
-            })
-            .sum::<i64>(),
-    )
+    let parsed_numbers = transpose(parsed_numbers);
+    Some(operate_all(operands, parsed_numbers))
 }
 
-pub fn part_two(input: &str) -> Option<i64> {
-    let (numbers_text, operations) = input.strip_suffix("\n").unwrap().rsplit_once("\n").unwrap();
-    let operations: Vec<char> = operations.chars().collect();
+pub fn part_two(input: &str) -> Option<u64> {
+    let (numbers_text, operands) = input.strip_suffix("\n").unwrap().rsplit_once("\n").unwrap();
+    let operand_chars: Vec<char> = operands.chars().collect();
+    let operands: Vec<char> = operand_chars.iter().copied().filter(|char| char != &' ').collect();
     let split_numbers: Vec<Vec<char>> = numbers_text
         .split("\n")
         .into_iter()
         .map(|line| line.chars().collect())
         .collect();
-    let mut operation_placements: Vec<usize> = operations
+    let mut operation_placements: Vec<usize> = operand_chars
         .iter()
         .enumerate()
         .filter(|(_, char)| char != &&' ')
         .map(|(index, _)| index)
         .collect();
-    operation_placements.push(operations.len() + 1);
+    operation_placements.push(operand_chars.len() + 1);
 
-    let mut sum: i64 = 0;
+    let mut numbers_list: Vec<Vec<u64>> = Vec::new();
+
     for operation_index in 0..operation_placements.len() - 1 {
-        let mut numbers: Vec<i64> = Vec::new();
+        let mut current_numbers: Vec<u64> = Vec::new();
         for index in
             operation_placements[operation_index]..operation_placements[operation_index + 1] - 1
         {
@@ -65,19 +79,11 @@ pub fn part_two(input: &str) -> Option<i64> {
             for number in &split_numbers {
                 current_number.push(number[index]);
             }
-            numbers.push(current_number.trim().parse().unwrap());
+            current_numbers.push(current_number.trim().parse().unwrap());
         }
-        match operations[operation_placements[operation_index]] {
-            '*' => {
-                sum += numbers.iter().copied().reduce(|a, b| a * b).unwrap();
-            }
-            '+' => {
-                sum += numbers.iter().sum::<i64>();
-            }
-            _ => panic![],
-        }
+        numbers_list.push(current_numbers);
     }
-    Some(sum)
+    Some(operate_all(operands, numbers_list))
 }
 
 #[cfg(test)]
