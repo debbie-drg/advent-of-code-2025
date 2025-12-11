6bc7b07e8929119e0b0ddac816190a3a68b72774
Debbie Dragoness <debbiedragoness@gmail.com>
2025-12-11 07:56:25 +0000

    Day 11 solved!

---
+++ src/bin/11.rs
@@ -0,0 +1,92 @@
+advent_of_code::solution!(11);
+
+use std::collections::HashMap;
+
+fn parse_node(input: &str) -> Option<(String, Vec<String>)> {
+    let (name, children) = input.split_once(":")?;
+    let children = children
+        .split(" ")
+        .filter(|name| name.len() > 0)
+        .map(|name| name.to_owned())
+        .collect();
+    Some((name.to_owned(), children))
+}
+
+fn parse_nodes(input: &str) -> HashMap<String, Vec<String>> {
+    input
+        .split("\n")
+        .map(|line| parse_node(line))
+        .filter_map(|x| x)
+        .collect()
+}
+
+fn count_paths(
+    boxes: &HashMap<String, Vec<String>>,
+    start_position: &str,
+    end_position: &str,
+) -> u64 {
+    let mut counts = HashMap::new();
+    if end_position != "out" {
+        counts.insert("out".to_owned(), 0);
+    }
+    count_paths_recursor(&boxes, start_position.to_owned(), end_position, &mut counts)
+}
+
+fn count_paths_recursor(
+    boxes: &HashMap<String, Vec<String>>,
+    current_position: String,
+    end_position: &str,
+    counts: &mut HashMap<String, u64>,
+) -> u64 {
+    let mut result: u64 = 0;
+    for child in boxes[&current_position].clone() {
+        if counts.contains_key(&child) {
+            result += counts[&child];
+        } else {
+            if &child == end_position {
+                result += 1;
+            } else {
+                result += count_paths_recursor(boxes, child, end_position, counts)
+            }
+        }
+    }
+    counts.insert(current_position, result);
+    result
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let boxes = parse_nodes(input);
+    Some(count_paths(&boxes, "you", "out"))
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let boxes = parse_nodes(input);
+    let server_fft = count_paths(&boxes, "svr", "fft");
+    let server_dac = count_paths(&boxes, "svr", "dac");
+    let dac_fft = count_paths(&boxes, "dac", "fft");
+    let fft_dac = count_paths(&boxes, "fft", "dac");
+    let dac_out = count_paths(&boxes, "dac", "out");
+    let fft_out = count_paths(&boxes, "fft", "out");
+    Some(server_fft * fft_dac * dac_out + server_dac * dac_fft * fft_out)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
+        assert_eq!(result, Some(5));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(2));
+    }
+}
